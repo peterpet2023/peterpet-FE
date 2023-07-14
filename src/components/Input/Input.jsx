f076aa60ec53b5852a849cea747792c881b40591
pakxe <pigkill40@naver.com>
2023-07-14 18:03:28 +0900

    feat: Input에 value, inChange 연결

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -6,13 +6,13 @@ const StyledInput = styled.input`
   padding: 16px;
   border-radius: 7px;
   border: 1px solid ${({ theme }) => theme.colors.darkGray};
-
+  font-size: 14px;
   // 이게 적용이 안되는 것 같습니다.
   ::placeholder {
     color: ${({ theme }) => theme.colors.darkGray};
   }
 `;
 
-export default function Input({ placeholder }) {
-  return <StyledInput placeholder={placeholder} />;
+export default function Input({ placeholder, value, onChange }) {
+  return <StyledInput value={value} onChange={onChange} placeholder={placeholder} />;
 }
