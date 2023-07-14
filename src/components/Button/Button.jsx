bd645f809261781cdf2c97b3ecdb69dd5373bb7a
pakxe <pigkill40@naver.com>
2023-07-14 15:42:13 +0900

    style: 버튼에 커서 효과 작성

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -7,7 +7,7 @@ const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.gray};
   border: none;
   color: ${({ theme }) => theme.colors.white};
-  
+  cursor: pointer;
 `;
 
 export default Button;
